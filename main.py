aac7120c7d2480c694e4f4cd02ac224b71ae32a3
Byron French <bfrench9098@gmail.com>
2023-04-08 10:21:28 -0600

    revised the todo app to loop after enter or list

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 #print("Enter a todo:")
 prompt_enter = 'Enter a todo (blank to exit):'
-prompt_ask = '(E)nter or (L)ist todos? (blank to exit):'
+prompt_ask = 'Enter or List todos? ([Ee]nter | [Ll]ist |  blank to exit:'
 
 todos = []
 
-response = input(prompt_ask)
-response.lower()
+exit = False
 
-while (not response.startswith('e') and not response.startswith('l')):
+while (exit == False):
     response = input(prompt_ask)
-    response.lower()
+    action = response.lower()
 
-if (response.startswith('e')):
+    if len(action) == 0:
+        action = 'exit'
+
+    match action:
+        case 'enter':
+            exit = True
+        case 'list':
+            exit = True
+        case 'exit':
+            exit = True
+            action = ''
+
+if (action.startswith('e')):
     response = input(prompt_enter)
 
     if len(response) > 0:
@@ -22,6 +33,14 @@ if (response.startswith('e')):
         if len(response) > 0:
             todos.append(response.capitalize())
 
-    print("\nYour todos: ", todos)
-else:
-    print ("Your todos: ", todos)
+    print('\nYour Todos:')
+    for item in todos:
+        print("\t", item)
+
+    # print("\nYour todos: ", todos)
+elif (action.startswith('l')):
+    print('\nYourTodos:')
+    for item in todos:
+        print("\t", item)
+
+    # print ("\nYour todos: ", todos)
