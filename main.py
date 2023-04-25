f6454db214465f7dac094822e0bc5295bafba571
Byron French <bfrench9098@gmail.com>
2023-04-25 17:29:35 -0600

    added file processing to the program to allow storage of todos.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os.path
+import sys
+
 # Very simple command line based menu to enter strings
 
 # When prompted enter either "enter" or "list" or press enter to exit.
@@ -18,7 +21,7 @@
 def listTodos():
     print('\nYour Todos:')
 
-    if (todos.__len__() > 0):
+    if (len(todos) > 0):
         itemNo = 0
 
         for idx, item in enumerate(todos):
@@ -86,6 +89,44 @@ def completeTodos():
                 listTodos()
         else:
             print('ERROR! - ', response, ' is not valid. Enter an item number from the list')
+
+#--------------------------------------------------
+#loadFromFile - Load todos from text file
+#--------------------------------------------------
+def loadFromFile(todos):
+    ### first see if the file exists - using default name "todos.txt"
+    myFile = 'todos.txt'
+    f = None
+
+    if os.path.exists(myFile):
+        ### if found then load todos[] from file
+        try:
+            f = open(myFile, "r+")
+        except:
+            print("ERROR! Could not open ", myFile)
+            sys.exit(1)
+
+        for line in f:
+            line = line.strip('\n')
+            todos.append(line)
+
+    return f
+
+#--------------------------------------------------
+#writeToFile - Write todos to a file
+#--------------------------------------------------
+def writeToFile(f, todos):
+    ### first see if the file exists - using default name "todos.txt"
+    myFile = 'todos.txt'
+
+    ### if found then truncate file and write any data in todos[]
+    if os.path.exists(myFile):
+        f.seek(0)
+        f.truncate()
+
+    for todo in todos:
+        todo = todo + '\n'
+        f.write(todo)
 ### END OF FUNCTIONS
 
 prompt_enter = 'Enter a todo (blank to exit):'
@@ -95,6 +136,10 @@ prompt_replace = 'Enter the replacement todo (blank to exit):'
 prompt_ask = 'Enter, Edit, Complete or List todos? (blank to exit):'
 
 todos = []
+
+f = loadFromFile(todos)
+
+print('PROGRAM START: todos loaded from file', len(todos), '\n')
 
 exit = False
 actionValid = False
@@ -116,6 +161,14 @@ while (exit == False):
         case 'complete':
             actionValid = True
         case 'exitApp':
+            try:
+                writeToFile(f, todos)
+            except:
+                print('ERROR! Error writing to todos file')
+                sys.exit(1)
+
+            print('\nPROGRAM END: todos written to file', len(todos), '\n')
+
             break
         case other:
             print('ERROR! That is not a valid option.')
