fc24d0c6dba2d064c364a0dafc6e860e2d3b4a35
Byron French <bfrench9098@gmail.com>
2023-05-29 10:04:02 -0600

    Remove large block of comments

--- passwordchecker.py
+++ passwordchecker.py
@@ -57,55 +57,6 @@ def checkPassword(password, requirements):
         result = True;
 
     return result
-
-# def checkForWeakPassword(password):
-#     # Requirements for a "weak" password
-#     minPWDLenWeak = 6
-#     minUpperCaseLettersWeak = 1
-#     minNumericCharsWeak = 1
-#     minSpecialCharsWeak = 0
-#
-#     weak = False;
-#     count = 0
-#
-#     # check password length
-#     if len(password) >= minPWDLenWeak:
-#         count = count + 1;
-#
-#     # check number of upper case characters
-#     upCaseCharCount = 0
-#
-#     for character in password:
-#         if re.match('[A-Z]', character):
-#             upCaseCharCount = upCaseCharCount + 1;
-#
-#     if upCaseCharCount >= minUpperCaseLettersWeak:
-#         count = count + 1;
-#
-#     # check number of numeric characters
-#     numCharCount = 0
-#
-#     for character in password:
-#         if character.isnumeric():
-#             numCharCount = numCharCount + 1;
-#
-#     if numCharCount >= minNumericCharsWeak:
-#         count = count + 1;
-#
-#     # check number of special characters
-#     specCharCount = 0
-#
-#     for character in password:
-#         if (re.match('[|^&+\-%*/=!>\.]', character)):
-#            specCharCount = specCharCount + 1;
-#
-#     if specCharCount >= minSpecialCharsWeak:
-#         count = count + 1;
-#
-#     if count == 4:
-#         weak = True;
-#
-#     return weak
 ### END FUNCTIONS
 
 ### request password
